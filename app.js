367217ac8495ff07e7dfcbbb42e28b2138748def
vijay <vijaykarthik9868@gmail.com>
2022-12-25 16:28:53 +0530

    stable db

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const url =
-  "mongodb+srv://vmongo:mongoDB@cluster0.8w8ijfl.mongodb.net/?retryWrites=true&w=majority";
+const mongodbURL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", true);
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongodbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const con = mongoose.connection;
 con.on("open", function () {
   console.log("connected...");
 });
+app.use(
+  cors({
+    origin: ("http://localhost:3000", "*"),
+  })
+);
 app.use(express.json());
 const alienRouter = require("./routes/alien");
 app.use("/aliens", alienRouter);
-
+app.get("/", (req, res) => {
+  res.send("CRUD Express app is running");
+});
 app.listen(9000, () => {
   console.log("Server started");
 });
